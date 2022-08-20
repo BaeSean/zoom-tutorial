7f936f61ea517c106599fdd33cd3f7086f511062
Seungyong Bae <seungyongbae@Seungyongui-MacBookAir.local>
2022-08-20 13:54:00 +0900

    Connect test Server and Browser w/ websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,19 @@
 // alert("hi");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+    console.log("Connected to Server");
+});
+
+socket.addEventListener("message", (message) => {
+    console.log("Server : " + message.data);
+});
+
+socket.addEventListener("close", () => {
+    console.log("Disconnect to Server");
+});
+
+setTimeout(() => {
+    socket.send("What sup");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,4 +11,23 @@ app.get("/", (req, res) => res.render("home"));
 // app.get("/*", (req, res) => res.render("home"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+//같은 서버(포트)에서 http, websocket 실행
+const server = http.createServer(app); //http server
+const wss = new WebSocket.Server({ server }); //websocket server
+
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser");
+    socket.on("close", () => {
+        console.log("Disconnect from the Browser");
+    })
+    socket.on("message", message => {
+        console.log("Browser : " + message);
+    });
+
+    socket.send("Hi");
+});
+
+server.listen(3000, handleListen);
